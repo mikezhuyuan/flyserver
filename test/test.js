f63c866dc4aba080d739e4857ca835c28170fc59
dragonpig <mikezhuyuan@hotmail.com>
2012-03-17 15:02:52 +0800

    some refactors & add parse body

--- test/test.js
+++ test/test.js
@@ -1,13 +1,21 @@
 var app = require('../');
 
 function init(route){
-    route('GET', ['/user/', { firstname: /\w+/ }, '-', { lastname: /\w+/}], function (req, res, next) {	    	    
-        res.end(JSON.stringify(req.params));
+    route('GET', ['/user/', {id:/\d+/}], function (req, res, next) {
+        var id = req.params.id;
+		res.writeHead(200, {"Content-Type":"text/html"});
+        res.end('<form action="/user/'+id+'", method="POST"><input name="name" ><input type="submit" value="submit"></form>');
+    });
+	
+	route('POST', ['/user/', {id:/\d+/}], function (req, res, next) {
+		var name = req.body.name;
+		res.end(name);
 	});
 }
 
-app.configure(function(use){
-    use(app.basic());
+app.configure(function (use) {
+    use(app.url());
+    use(app.body());
     use(app.static(__dirname));
     use(app.router(init));
     use(app.error());
